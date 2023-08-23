10525a0137626e84900942bb18683d6b9d094ea0
Ítalo Eugênio <italoeugenio539@gmail.com>
2023-08-23 14:50:07 -0300

    add exercise 3 and 4

--- Lista 1/atividade2/exercicio3.c
+++ Lista 1/atividade2/exercicio3.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /*Exercício 3 - Verificar se um elemento existe no array Escreva uma função que recebe um array de strings e uma string de busca,
  e retorna "1" se a string de busca existe no array, ou "0" caso não exista.*/
 
-int elementoExisteNoArray(const char *array[], int tamanhoArray, const char *elementoProcurado)
+int busca_string(const char *array[], int tamanhoArray, const char *elementoProcurado)
 {
     for (int i = 0; i < tamanhoArray; i++)
     {
         if (strcmp(array[i], elementoProcurado) == 0)
         {
-            return 1; // Elemento encontrado
+            return 1;
         }
     }
-    return 0; // Elemento não encontrado
+    return 0;
 }
 
 int main()
 {
     const char *array[] = {"texto", "J", "EDA"};
-    const char *elementoProcurado = "EDO";
+    const char *elementoProcurado = "EDO"; //EDO
     int tamanhoArray = sizeof(array) / sizeof(array[0]);
 
-    if (elementoExisteNoArray(array, tamanhoArray, elementoProcurado))
-    {
-        printf("O elemento %s foi encontrado no array.\n", elementoProcurado);
-    }
-    else
-    {
-        printf("O elemento %s não foi encontrado no array.\n", elementoProcurado);
-    }
+    int resultado = busca_string(array, tamanhoArray, elementoProcurado);
+
+    printf("%s\n", elementoProcurado);
+    printf("%d\n", resultado);
 
     return 0;
 }
--- Lista 1/atividade2/exercicio4.c
+++ Lista 1/atividade2/exercicio4.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/*Exercício 4 - Contar a quantidade de ocorrências de um elemento no array. Escreva uma função que recebe um array de números inteiros e um número inteiro de busca, e retorna a quantidade de vezes que o número de busca aparece no array.
+
+Exemplo: conta_int_array([5, 7, 9, 6, 7, 11, 6, 5], 7)
+Saída: 2*/
+
+int conta_int_array(int array[], int tamanho, int numero_busca)
+{
+    int contador = 0;
+    int i = 0;
+
+    while (i < tamanho)
+    {
+        if (array[i] == numero_busca)
+        {
+            contador++;
+        }
+        i++;
+    }
+
+    return contador;
+}
+
+int main()
+{
+    int array_exemplo[] = {5,7,9,6,7,11,6,5};
+    int tamanho_array = sizeof(array_exemplo) / sizeof(array_exemplo[0]);
+    int numero_busca_exemplo = 7;
+
+    int quantidade_ocorrencias = conta_int_array(array_exemplo, tamanho_array, numero_busca_exemplo);
+    printf("Saida: %d\n", quantidade_ocorrencias);
+
+    return 0;
+}
