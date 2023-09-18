205278f8b0327aa1634c9d156af2eb0545ccf12d
Ítalo Eugênio <italoeugenio539@gmail.com>
2023-09-18 16:27:13 -0300

    fixed error

--- Lista 2/atividade 4/main.c
+++ Lista 2/atividade 4/main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include "bubble_sort.h"
 
+
+/*Exemplo:
+./main 54 26 93 17 77 31 44 55 20
+Saída: 17 20 26 31 44 54 55 77 93*/
+
 int main(int argc, char *argv[]) {
-    int arraySize = argc - 2;
+    int arraySize = argc - 1;
     int vet[arraySize];
 
     for (int i = 0; i < arraySize; i++) {
-        int value = atoi(argv[i + 2]);
+        int value = atoi(argv[i + 1]);
         vet[i] = value;
     }
 
@@ -21,5 +26,5 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// gcc main.c -o main
+// gcc main.c bubble_sort.c -o main
 //./main 54 26 93 17 77 31 44 55 20
